277473733f9002b19f4d3dc8e1895d2116c768c7
Anson Chen <s941314g@yahoo.com.tw>
2020-02-11 14:53:25 +0800

    set ios depoly version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "XIPalette",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v10)],
     products: [
         .library(name: "XIPalette", targets: ["XIPalette"])
     ],
